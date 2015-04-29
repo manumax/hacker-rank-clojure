b927e137f8949ba8d0c998625b82489c3b24c76f
Manuele Mion <manuele.mion@gmail.com>
2015-04-29 18:54:20 +0200

    Now it works properly.

--- src/hacker_rank_clojure/sorting/insertion_sort.clj
+++ src/hacker_rank_clojure/sorting/insertion_sort.clj
@@ -1,11 +1,28 @@
 (ns hacker-rank-clojure.sorting.insertion-sort)
 
-(defn part-one [starting-list]
-  (loop [v        (peek starting-list)
-         to-parse (pop starting-list)
-         parsed   '()]
-    (println (concat to-parse (vector v) parsed))
+(defn parse-int [string]
+  (Integer. string))
+
+(defn read-values []
+  (do
+    (read-line)
+    (into [] (map parse-int (clojure.string/split (read-line) #" ")))))
+
+(defn part-one-step [v to-parse parsed]
+  (lazy-seq
     (let [next-item (peek to-parse)]
       (if (or (nil? next-item) (<= next-item v))
-        (concat to-parse (vector v) parsed)
-        (recur v (pop to-parse) (conj parsed next-item))))))
+        [(concat to-parse [v] parsed)]
+        (cons (concat to-parse [(peek to-parse)] parsed)
+              (part-one-step v (pop to-parse) (conj parsed next-item)))))))
+
+(defn part-one [starting-list]
+  (part-one-step (peek starting-list) (pop starting-list) '()))
+
+(defn run-part-one
+  [starting-list]
+  (doall (map (comp println (partial clojure.string/join " ")) (part-one starting-list))))
+
+(defn -main
+  []
+  (run-part-one [2 4 6 8 3]))
