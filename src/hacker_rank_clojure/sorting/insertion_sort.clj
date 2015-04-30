9437bbc71b11ec3f2deeb58828895925b13b417d
Manuele Mion <manuele.mion@gmail.com>
2015-04-30 18:28:07 +0200

    To be done.

--- src/hacker_rank_clojure/sorting/insertion_sort.clj
+++ src/hacker_rank_clojure/sorting/insertion_sort.clj
@@ -1,5 +1,6 @@
-(ns hacker-rank-clojure.sorting.insertion-sort
-  (:require [hacker-rank-clojure.utils :as utils]))
+(ns hacker-rank-clojure.sorting.insertion-sort)
+
+;; https://www.hackerrank.com/challenges/insertionsort1
 
 (defn insertion-sort-steps
   ([starting-list]
@@ -12,3 +13,16 @@
          [(concat to-parse [v] parsed)]
          (cons (concat to-parse [(peek to-parse)] parsed)
                (insertion-sort-steps v (pop to-parse) (conj parsed next-item))))))))
+
+;; https://www.hackerrank.com/challenges/insertionsort2
+
+(defn insertion-sort [list]
+  (loop [to-order      (take 2 list)
+         to-be-ordered (drop 2 list)
+         acc           []]
+    (if (seq to-be-ordered)
+      (recur (conj (into [] (last (insertion-sort-steps to-order))) (first to-be-ordered))
+             (rest to-be-ordered)
+             (conj acc (concat to-order to-be-ordered))))))
+
+
--- test/hacker_rank_clojure/sorting/insertion_sort_test.clj
+++ test/hacker_rank_clojure/sorting/insertion_sort_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [hacker-rank-clojure.sorting.insertion-sort :refer :all]))
 
+;; https://www.hackerrank.com/challenges/insertionsort1
+
 (deftest test-first-step
   (testing "Check first step is ok"
     (is (= [2 4 6 8 8] (first (insertion-sort-steps [2 4 6 8 3]))))))
@@ -21,3 +23,14 @@
 (deftest test-one-element-list
   (testing "Should be ok if list has only one item"
     (is (= [1] (last (insertion-sort-steps [1]))))))
+
+;; https://www.hackerrank.com/challenges/insertionsort2
+
+(deftest test-sample-input-works
+  (testing "Sample Input should work properly"
+    (let [expected [[1 4 3 5 6 2]
+                    [1 3 4 5 6 2]
+                    [1 3 4 5 6 2]
+                    [1 3 4 5 6 2]
+                    [1 2 3 4 5 6]]]
+      (is (= expected (insertion-sort [1 4 3 5 6 2]))))))
